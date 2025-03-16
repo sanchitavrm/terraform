cc28fe8690d8a7bb8d36a1d7cd259d2e559a11bb
sanchitavrm <sanchitavrm@gmail.com>
2025-03-16 10:03:57 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,12 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAQ4NXP3NYTUIWXCW6-1"
-  secret_key = "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-1"
+  access_key = "first"
+  secret_key = "second"
 }
+
+AKIAQ4NXP3NYTUIWXCW6
+
+9JwyBUBRJNEK5SbGFxy724NovWKYENODYds5FYx1
 
 resource "aws_instance" "Jenkins" {
     ami = "ami-0cb91c7de36eed2cb"
