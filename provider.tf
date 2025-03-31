2646f5d25bf33d0605ed803a7655e49f4b63ad9d
sanchitavrm <sanchitavrm@gmail.com>
2025-03-31 16:11:20 -0600

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = "us-east-2"  # your desired region
+  # credentials will be automatically loaded from ~/.aws/credentials
+  # AKIAQ4NXP3NYTUIWXCW6
+  # 9JwyBUBRJNEK5SbGFxy724NovWKYENODYds5FYx1
+} 
