65288a2f1cd3f3d94d14baaca95560962d854aef
Sanchita Verma <sanchitavrm@gmail.com>
2025-04-20 14:02:03 -0600

    updated

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "Demo" {
-    ami = "ami-0cb91c7de36eed2cb"
-    instance_type = "t2.micro"
+    ami = var.ami
+    instance_type = var.instance_type
 }
---
+++ modules/ec2/variables.tf
@@ -0,0 +1,6 @@
+variable "ami" {
+    ami = "ami-0cb91c7de36eed2cb"
+}
+variable "instance_type" {
+    instance_type = "t2.micro"
+}
